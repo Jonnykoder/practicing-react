72898bfc6ed9a924695cc04189126dd28064f746
John Eric <azores.johneric28@gmail.com>
2022-04-18 11:45:30 +0800

    first commit in repo

---
+++ 02_First_React/index.js
@@ -0,0 +1,25 @@
+ReactDOM.render(
+    <div>
+        <div className="title">
+            <h1>These are my tasks on react</h1>
+        </div>
+        <div className="subheading">
+        </div>
+        <div className = "contents">
+            <div className="cont1">
+                <ul>
+                    <li> 
+                        <h3>Learn the Fundamentals</h3>
+                    </li>
+                </ul>
+                <ol>
+                    <li>Learn the syntax , usage of the built in library</li>
+                    <li>Create some small projects</li>
+                    <li>Apply the learning in real world projects</li>
+                    <li>Create an application that are deployed in the production</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+    
+    ,document.getElementById("root"))
